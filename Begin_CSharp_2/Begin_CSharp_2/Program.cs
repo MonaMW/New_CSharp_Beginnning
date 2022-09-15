a9623c20f295bcb7d510f6ed261c34cabb9a50b3
Walter, Mona <mona.walter@tecalliance.net>
2022-09-15 11:26:16 +0200

    Started new project

---
+++ Begin_CSharp_2/Begin_CSharp_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin_CSharp_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first_number = 1;
+            int second_number = 2;
+
+            int sum = first_number + second_number;
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
