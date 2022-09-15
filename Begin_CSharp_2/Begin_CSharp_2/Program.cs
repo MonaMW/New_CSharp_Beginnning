8da9d281e35237a2280e32dca815a6e9ae7690b0
Walter, Mona <mona.walter@tecalliance.net>
2022-09-15 14:11:42 +0200

    For-Schleife - Aufgabe 1

--- Begin_CSharp_2/Begin_CSharp_2/Program.cs
+++ Begin_CSharp_2/Begin_CSharp_2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,15 @@ namespace Begin_CSharp_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bitte schreibe einen Text ein");
-            string entry = Console.ReadLine();
+            Console.WriteLine("Bitte gebe eine Zahl ein");
+            int entry = Convert.ToInt32(Console.ReadLine());
+            int j = 0;
 
-            string to_upper_case = entry.ToUpper();
-            string to_lower_case = entry.ToLower();
-
-            Console.WriteLine("Upper Cases: " + to_upper_case);
-            Console.WriteLine("Lower Cases: " + to_lower_case);
+            for(int i = 0; i < entry; i++)
+            {
+                j = i + j;
+                Console.WriteLine(j);
+            }
             
             Console.ReadLine();
         }
