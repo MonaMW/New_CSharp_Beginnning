c5201f92c489211a3ea097711c43615ac8ba9fe8
Walter, Mona <mona.walter@tecalliance.net>
2022-09-16 11:34:39 +0200

    Add first method

--- Begin_CSharp_2/Begin_CSharp_2/Program.cs
+++ Begin_CSharp_2/Begin_CSharp_2/Program.cs
@@ -11,28 +11,23 @@ namespace Begin_CSharp_2
     {
         static void Main(string[] args)
         {
-            List<string> names = new List<string>();
-            int entries = Convert.ToInt32(Console.ReadLine());
-            string[] namenArray = new string[entries];
-            Console.Clear();
+            Console.WriteLine("Wir holen uns jetzt eine Zahl aus Number()");
+            List<string> names = Number();
+            foreach(var name in names)
+            {
+                Console.WriteLine(name);
+            }
             
-            for(int i = 0; i < entries; i++)
-            {
-                names.Add(Console.ReadLine());
-                namenArray[i] = i;
-                Console.Clear();
-            }
-
-            //Einen Eintrag löschen
-            names.RemoveAt(3);
-            namenArray[3] = "";
-
-            for(int i = 0; i < names.Count; i++)
-            {
-                Console.WriteLine(names[i]);
-            }
-
             Console.ReadLine();
+        }
+
+        static List<string> Number()
+        {
+            List<string> name = new List<string>();
+            name.Add("ABC");
+
+            //Rückgabevariable
+            return name;
         }
     }
 }
