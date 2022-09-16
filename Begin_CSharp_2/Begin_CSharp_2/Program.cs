a5a4663d99d62b0ead5c17f0e240939ee69bfa6c
Walter, Mona <mona.walter@tecalliance.net>
2022-09-16 11:21:13 +0200

    Remov entry in list / array

--- Begin_CSharp_2/Begin_CSharp_2/Program.cs
+++ Begin_CSharp_2/Begin_CSharp_2/Program.cs
@@ -13,13 +13,19 @@ namespace Begin_CSharp_2
         {
             List<string> names = new List<string>();
             int entries = Convert.ToInt32(Console.ReadLine());
+            string[] namenArray = new string[entries];
             Console.Clear();
             
             for(int i = 0; i < entries; i++)
             {
                 names.Add(Console.ReadLine());
+                namenArray[i] = i;
                 Console.Clear();
             }
+
+            //Einen Eintrag löschen
+            names.RemoveAt(3);
+            namenArray[3] = "";
 
             for(int i = 0; i < names.Count; i++)
             {
