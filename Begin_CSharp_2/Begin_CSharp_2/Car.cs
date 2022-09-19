b1741f51b41e6bcaeb98cfa7b501c8255627b768
Walter, Mona <mona.walter@tecalliance.net>
2022-09-19 09:31:14 +0200

    Cars v1

---
+++ Begin_CSharp_2/Begin_CSharp_2/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin_CSharp_2
+{
+    public class Car
+    {
+        public int TypeId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Typename { get; set; }
+        public int CCM { get; set; }
+        public int Tyres { get; set; }
+        public string Color { get; set; }
+
+        public void PrintCarInformation()
+        {
+            Console.WriteLine($"Mein Fahrzeug ist ein {Make} {Model} {Typename} mit einem Hubraum von {CCM} in der Farbe {Color} mit {Tyres} Reifen.");
+        }
+    }
+}
--- Begin_CSharp_2/Begin_CSharp_2/Program.cs
+++ Begin_CSharp_2/Begin_CSharp_2/Program.cs
@@ -11,71 +11,29 @@ namespace Begin_CSharp_2
     {
         static void Main(string[] args)
         {
-            int budget = 10000;
-            Console.WriteLine("Budget ist " + budget);
+            Car c1 = new Car();
+            c1.TypeId = 13494;
+            c1.Make = "Audi";
+            c1.Model = "A3";
+            c1.Typename = "2.0 TDI";
+            c1.CCM = 1900;
+            c1.Tyres = 4;
+            c1.Color = "rot";
 
-            Console.WriteLine("Setze deinen Einsatz");
-            int einsatz = Convert.ToInt32(Console.ReadLine());
+            //c1.PrintCarInformation();
 
-            bool gewonnen = true;
-            
-            Random k = new Random();
-            int random_zahl;
+            Car c2 = new Car();
+            c2.TypeId = 55543;
+            c2.Make = "Volkswagen";
+            c2.Model = "Golf 5";
+            c2.Typename = "GTI";
+            c2.CCM = 2000;
+            c2.Tyres = 4;
+            c2.Color = "black";
 
-            int count = 0;
+            //c2.PrintCarInformation();
 
-            do
-            {
-                random_zahl = k.Next(0, 36);
-
-                if (budget >= 10000 && gewonnen)
-                {
-                    einsatz += 10;
-                    Console.WriteLine("Einsatz 10$");
-                    budget = budget - einsatz;
-                }
-                else if (budget >= 10000 && !gewonnen)
-                {
-                    einsatz += 20;
-                    Console.WriteLine("Einsatz 20$");
-                    budget = budget - einsatz;
-                }
-
-                //Gesamtverlust wird verdoppelt
-                if (budget < 10000)
-                {
-                    einsatz = (10000 - budget) * 2;
-                }
-
-                //Budget reicht nicht für geplanten Zug
-                if (einsatz > budget)
-                    einsatz = budget;
-
-                //wenn k gerade und größer 0
-                if (random_zahl > 0 && random_zahl % 2 == 0)
-                {
-                    gewonnen = true;
-                    budget += einsatz * 2;
-                    Console.WriteLine("Sie erhalten " + einsatz * 2);
-                }
-                //wenn k == 0
-                else if (random_zahl == 0)
-                {
-                    Console.WriteLine("Ihr Einsatz bleibt bestehen.");
-                }
-                //wenn k ungerade ist
-                else if (!(random_zahl % 2 == 0))
-                {
-                    budget = budget - einsatz;
-                    Console.WriteLine("Sie haben Ihren Einsatz verloren.");
-                    gewonnen = false;
-                }
-                count++;
-                Console.WriteLine("Random-Zahl: " + random_zahl);
-
-            } while (budget > 0 && gewonnen);
-
-            Console.WriteLine("Anzahl Spiele: " + count);
+            //Console.WriteLine($"Mein Fahrzeug ist ein {c1.Make} {c1.Model} {c1.Typename} mit einem Hubraum von {c1.CCM} in der Farbe {c1.Color} mit {c1.Tyres} Reifen.");
             Console.ReadLine();
         }
     }
