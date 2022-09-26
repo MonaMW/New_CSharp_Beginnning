0e173987d39b4fb179baf6e7bceb32271c2feb06
Walter, Mona <mona.walter@tecalliance.net>
2022-09-26 14:06:47 +0200

    Geometrie Wiederholung

--- Begin_CSharp_2/Begin_CSharp_2/Program.cs
+++ Begin_CSharp_2/Begin_CSharp_2/Program.cs
@@ -11,27 +11,7 @@ namespace Begin_CSharp_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wie viele Einträge soll das Array haben?");
-            int count = Convert.ToInt32(Console.ReadLine());
-            float[] ints = new float[count];
-            float for_loop = 0;
-            float foreach_loop = 0;
-
-            for(int i = 0; i < ints.Length; i++)
-            {
-                ints[i] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            for(int i = 0; i < ints.Length; i++)
-            {
-                for_loop += ints[i];
-            }
-
-            foreach(int i in ints) 
-            {
-                foreach_loop += ints[i];
-            }
-
+            
 
             Console.ReadLine();
         }
